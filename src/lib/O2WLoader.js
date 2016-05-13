7aa484f520865ffae85d4ad85d3223eeec7d5352
gargsms <garg_sms@yahoo.in>
2016-05-13 21:57:03 +0530

    Large commit - Fix convention, promisify rendering process

--- src/lib/O2WLoader.js
+++ src/lib/O2WLoader.js
@@ -8,29 +8,37 @@ THREE.O2WLoader = function ( manager ) {
 
   this.objects = {
 
-    /**
-     * 3 : Vector 3D. These are origin vectors which can be referenced later
-     *
-     * 11 : Triangles. A group of triangles with similar properties.
-     *
-     * 12: Triangle_Strip, Triangle_Fan or Convex_Polygon. Doesn't really
-     *     matter much because they are identical to parse and draw.
-     *     For each of the objects in this list, we will add a type property
-     */
-
-    '3': [ ],
-
-    '11': [ ],
-
-    '12': [ ]
-
   };
 
-}
+};
 
 THREE.O2WLoader.prototype = {
 
   constructor: THREE.O2WLoader,
+
+  getBlank: function ( ) {
+
+    return {
+
+      /**
+       * 3 : Vector 3D. These are origin vectors which can be referenced later
+       *
+       * 11 : Triangles. A group of triangles with similar properties.
+       *
+       * 12: Triangle_Strip, Triangle_Fan or Convex_Polygon. Doesn't really
+       *     matter much because they are identical to parse and draw.
+       *     For each of the objects in this list, we will add a type property
+       */
+
+      '3': [ ],
+
+      '11': [ ],
+
+      '12': [ ]
+
+    };
+
+  },
 
   getVector3D: function ( x, y, z ) {
 
@@ -43,8 +51,6 @@ THREE.O2WLoader.prototype = {
   },
 
   getPrimitive: function ( type, ambient, diffuse, indices ) {
-
-    var dest = type === 11 ? 11 : 12;
 
     var primitive = [ ];
 
@@ -110,7 +116,7 @@ THREE.O2WLoader.prototype = {
       break;
     }
 
-    this.objects[ dest ].push( primitive );
+    return primitive;
 
   },
 
@@ -132,127 +138,135 @@ THREE.O2WLoader.prototype = {
       ',' + map.b + ')' );
   },
 
-  getVectorByIndex: function ( index ) {
-    var vertices = this.objects[ '3' ];
+  getVectorByIndex: function ( url, index ) {
+    var vertices = this.objects[ url ][ '3' ];
     return new THREE.Vector3( vertices[ index ][ 0 ],
       vertices[ index ][ 1 ],
       vertices[ index ][ 2 ] );
   },
 
-  load: function ( url, onProgress, onError ) {
+  load: function ( url, onProgress ) {
 
     THREE.Cache.enabled = true;
 
-    var loader = new THREE.XHRLoader( );
+    var scope = this;
 
-    /**
-     * We define the responseType as arraybuffer because we are handling
-     * binary data
-     * TODO: Discuss on CORS
-     */
+    var promisify = new Promise( function ( res, rej ) {
 
-    loader.setResponseType( 'arraybuffer' );
+      var loader = new THREE.XHRLoader( );
 
-    // Load the file in THREE.Cache
-    loader.load( url, function ( ) {}, onProgress, onError );
+      /**
+       * We define the responseType as arraybuffer because we are handling
+       * binary data
+       */
+      loader.setResponseType( 'arraybuffer' );
+
+      // Load the file in THREE.Cache
+      loader.load( url, function ( ) {
+        scope.objects[ url ] = scope.getBlank( );
+        res( );
+      }, onProgress, function ( ) {
+        rej( );
+      } );
+
+    } );
+
+    return promisify;
 
   },
 
   parse: function ( url ) {
 
+    var RECURSE_MAX = 2500;
     var scope = this;
 
-    var openPromise = new Promise( function ( res, rej ) {
+    var promisify = new Promise( function ( res, rej ) {
+
       var file = THREE.Cache.get( url );
-      if ( file ) {
-        res( file );
-      } else {
-        rej( );
-      }
-    } );
 
-    openPromise.then( function ( file ) {
+      /**
+       * Get a view on this ArrayBuffer
+       */
 
-        var RECURSE_MAX = 2500;
+      var view = new DataView( file );
 
-        /**
-         * Get a view on this ArrayBuffer
-         */
+      var count = 0,
+        offset = 0,
+        iterator = 0;
 
-        var view = new DataView( file );
+      var tempView;
 
-        var count = 0,
-          offset = 0,
-          iterator = 0;
+      /**
+       * Counter to be reset at RECURSE_MAX for the repeat function
+       */
+      var recurseCount = 0;
 
-        var tempView;
+      /**
+       * Begin parsing the file
+       * Start with zero offset and work on the way up
+       * Based on the object type, calculate the distance to another object
+       * Insert relevant data into this.objects
+       */
 
-        /**
-         * Counter to be reset at RECURSE_MAX for the repeat function
-         */
-        var recurseCount = 0;
+      var repeat = function ( offset ) {
 
-        /**
-         * Begin parsing the file
-         * Start with zero offset and work on the way up
-         * Based on the object type, calculate the distance to another object
-         * Insert relevant data into this.objects
-         */
+        recurseCount++;
 
-        var repeat = function ( offset ) {
+        if ( offset >= view.byteLength ) {
+          // Clear the Cached file
+          THREE.Cache.remove( url );
+          res( );
+          return;
+        }
 
-          recurseCount++;
+        var objectType = view.getUint8( offset++ ),
+          dest = 0;
 
-          if ( offset >= view.byteLength ) {
-            // Clear the Cached file
-            THREE.Cache.remove( url );
-            return;
+        switch ( objectType ) {
+
+        case 3:
+
+          // This is the count of vertices
+          count = view.getUint8( offset++ );
+
+          for ( iterator = 0; iterator < count; iterator++ ) {
+
+            scope.objects[ url ][ '3' ].push( scope.getVector3D(
+              scope.getInt24( view, offset ),
+              scope.getInt24( view, offset + 3 ),
+              scope.getInt24( view, offset + 6 )
+            ) );
+
+            offset += 9;
+
           }
 
-          var objectType = view.getUint8( offset++ );
-
-          switch ( objectType ) {
-
-          case 3:
-
-            // This is the count of vertices
-            count = view.getUint8( offset++ );
-
-            for ( iterator = 0; iterator < count; iterator++ ) {
-
-              scope.objects[ '3' ].push( scope.getVector3D(
-                scope.getInt24( view, offset ),
-                scope.getInt24( view, offset + 3 ),
-                scope.getInt24( view, offset + 6 )
-              ) );
-
-              offset += 9;
-
-            }
-
-            /**
-             * Call repeat with the incremented offset
-             */
-            if ( RECURSE_MAX > recurseCount ) {
+          /**
+           * Call repeat with the incremented offset
+           */
+          if ( RECURSE_MAX > recurseCount ) {
+            repeat( offset );
+          } else {
+            setTimeout( function ( ) {
+              recurseCount = 0;
               repeat( offset );
-            } else {
-              setTimeout( function ( ) {
-                recurseCount = 0;
-                repeat( offset );
-              }, 0 );
-            }
+            }, 0 );
+          }
 
-            break;
+          break;
 
-          case 11:
-          case 12:
-          case 13:
-          case 14:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
 
-            count = view.getUint8( offset + 7 );
+          count = view.getUint8( offset + 7 );
 
-            tempView = new DataView( file, offset + 8, count * 2 );
+          tempView = new DataView( file, offset + 8, count * 2 );
 
+          dest = objectType === 11 ? 11 : 12;
+
+          scope.objects[ url ][ dest ].push(
             scope.getPrimitive( objectType, {
               r: view.getUint8( offset + 1 ),
               g: view.getUint8( offset + 2 ),
@@ -261,90 +275,104 @@ THREE.O2WLoader.prototype = {
               r: view.getUint8( offset + 4 ),
               g: view.getUint8( offset + 5 ),
               b: view.getUint8( offset + 6 )
-            }, tempView );
+            }, tempView ) );
 
-            offset += count * 2 + 8;
+          offset += count * 2 + 8;
 
-            if ( RECURSE_MAX > recurseCount ) {
+          if ( RECURSE_MAX > recurseCount ) {
+            repeat( offset );
+          } else {
+            setTimeout( function ( ) {
+              recurseCount = 0;
               repeat( offset );
-            } else {
-              setTimeout( function ( ) {
-                recurseCount = 0;
-                repeat( offset );
-              }, 0 );
-            }
-
-            break;
-
-          default:
-
-            console.error( 'Bad block ' + objectType + ' at offset ' + offset );
-
+            }, 0 );
           }
-        };
 
-        repeat( offset );
+          break;
 
-      },
-      function ( ) {
-        console.log( ':(' );
-      } );
+        default:
+
+          rej( objectType, offset );
+
+        }
+      };
+
+      repeat( offset );
+
+
+    } );
+
+    return promisify;
+
   },
 
-  render: function ( ) {
+  render: function ( url, scene ) {
 
     var scope = this;
 
-    var renderedObject = new THREE.Object3D( ),
-      geo, mat, mesh,
-      vertices = scope.objects[ '3' ];
+    if ( url ) {
+      scope.load( url )
+        .then( function ( ) {
+          scope.parse( url )
+            .then( function ( ) {
+              var renderedObject = new THREE.Object3D( ),
+                geo, mat, mesh,
+                vertices = scope.objects[ url ][ '3' ];
 
-    function drawPrimitive( primitive ) {
-      primitive.forEach( function ( group ) {
+              function drawPrimitive( primitive ) {
+                primitive.forEach( function ( group ) {
 
-        geo = new THREE.Geometry( );
+                  geo = new THREE.Geometry( );
 
-        group.forEach( function ( indexArray, i ) {
+                  group.forEach( function ( indexArray, i ) {
 
-          geo.vertices.push( scope.getVectorByIndex( indexArray[ 0 ] ) );
-          geo.vertices.push( scope.getVectorByIndex( indexArray[ 1 ] ) );
-          geo.vertices.push( scope.getVectorByIndex( indexArray[ 2 ] ) );
+                    geo.vertices.push( scope.getVectorByIndex( url, indexArray[ 0 ] ) );
+                    geo.vertices.push( scope.getVectorByIndex( url, indexArray[ 1 ] ) );
+                    geo.vertices.push( scope.getVectorByIndex( url, indexArray[ 2 ] ) );
 
-          geo.faces.push( new THREE.Face3( 3 * i + 1, 3 * i + 0, 3 * i + 2 ) );
+                    geo.faces.push( new THREE.Face3( 3 * i + 1, 3 * i + 0, 3 * i + 2 ) );
 
+                  } );
+
+                  geo.computeFaceNormals( );
+
+                  mat = new THREE.MeshBasicMaterial( {
+                    color: scope.getColorByRGB( group.diffuse )
+                  } );
+
+                  mesh = new THREE.Mesh( geo, mat );
+
+                  renderedObject.add( mesh );
+
+                } );
+              }
+
+              // Iterate over the primitives and append each to the renderedObject
+              if ( scope.objects[ url ][ '11' ].length ) {
+
+                drawPrimitive( scope.objects[ url ][ '11' ] );
+
+              }
+
+              // TODO: Find if TriangleStripDrawMode, TriangleFanDrawMode are of any use
+              // Optimize this block, or merge it in the block above if no distinction
+              // in case of TriangleStrip or TriangleFan than just Triangles
+              if ( scope.objects[ url ][ '12' ].length ) {
+
+                drawPrimitive( scope.objects[ url ][ '12' ] );
+
+              }
+
+              scene.add( renderedObject );
+
+            }, function ( objectType, offset ) {
+              console.error( 'Bad object ' + objectType + ' at offset ' + offset );
+            } );
+        }, function ( ) {
+          console.error( 'Failed to load the file' );
         } );
-
-        geo.computeFaceNormals( );
-
-        mat = new THREE.MeshBasicMaterial( {
-          color: scope.getColorByRGB( group.diffuse )
-        } );
-
-        mesh = new THREE.Mesh( geo, mat );
-
-        renderedObject.add( mesh );
-
-      } );
     }
-
-    // Iterate over the primitives and append each to the renderedObject
-    if ( scope.objects[ '11' ].length ) {
-
-      drawPrimitive( scope.objects[ '11' ] );
-
-    }
-
-    // TODO: Find if TriangleStripDrawMode, TriangleFanDrawMode are of any use
-    // Optimize this block, or merge it in the block above if no distinction
-    // in case of TriangleStrip or TriangleFan than just Triangles
-    if ( scope.objects[ '12' ].length ) {
-
-      drawPrimitive( scope.objects[ '12' ] );
-
-    }
-
-    return renderedObject;
 
   }
 
-}
+};
